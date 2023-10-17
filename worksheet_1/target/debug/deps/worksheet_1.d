02b6be38acf955afe3e8900006d6e03f930eee39
LAPTOP-56OHRB4G\tahak <tahakhalfe1024@gmail.com>
2023-10-17 13:30:20 +0500

    initial commit

---
+++ worksheet_1/target/debug/deps/worksheet_1.d
@@ -0,0 +1,5 @@
+C:\Users\tahak\OneDrive\Desktop\rust_worksheet\worksheet_1\target\debug\deps\worksheet_1.exe: src\main.rs
+
+C:\Users\tahak\OneDrive\Desktop\rust_worksheet\worksheet_1\target\debug\deps\worksheet_1.d: src\main.rs
+
+src\main.rs:
---
+++ worksheet_1/target/debug/worksheet_1.d
@@ -0,0 +1,1 @@
+C:\Users\tahak\OneDrive\Desktop\rust_worksheet\worksheet_1\target\debug\worksheet_1.exe: C:\Users\tahak\OneDrive\Desktop\rust_worksheet\worksheet_1\src\main.rs
